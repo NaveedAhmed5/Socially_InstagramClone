482e340856dad804611224fc1e751e5fbe74ad72
Naveed Ahmed <i220889@nu.edu.pk>
2025-09-06 18:45:33 +0500

    The Input Activity is created and The SignUp Activity is started

--- app/src/main/java/com/example/assignment1/signup.kt
+++ app/src/main/java/com/example/assignment1/signup.kt
@@ -5,27 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.content.Intent
-import android.widget.Button
 
-class LoginActivity : AppCompatActivity() {
+class signup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_signup)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val btnSignUp = findViewById<Button>(R.id.signupBtn)
-
-        btnSignUp.setOnClickListener {
-            val intentSignup = Intent(this, signup::class.java)
-            startActivity(intentSignup)
-        }
-
-
     }
 }
